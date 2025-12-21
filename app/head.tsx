1a69fcf64dad7c3be4180cc47641de46469efe16
E-cell SVCE <devnull@ecellsvce.in>
2025-12-21 19:17:09 +0530

    Add google site verification meta (app/head.tsx)

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Head() {
+  return (
+    <>
+      <title>E - Cell SVCE | Entrepreneurship Cell</title>
+      <meta name="google-site-verification" content="ZgGe9C-HXqLHJExhXlQ8IS7nHLipeslwiHoQW_el9js" />
+    </>
+  )
+}
